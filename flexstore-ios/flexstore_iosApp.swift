51d32ad32e7330a8a80256f58374121a714e4a25
Krisna Pranav <68631244+krishpranav@users.noreply.github.com>
2024-12-31 19:22:46 +0530

    add files via upload

---
+++ flexstore-ios/flexstore_iosApp.swift
@@ -0,0 +1,17 @@
+//
+//  flexstore_iosApp.swift
+//  flexstore-ios
+//
+//  Created by Krisna Pranav on 31/12/24.
+//
+
+import SwiftUI
+
+@main
+struct flexstore_iosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
