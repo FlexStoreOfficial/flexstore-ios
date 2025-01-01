7a9ddeecc88cd1a202bed926aa5c08df4b50e51d
Krisna Pranav <68631244+krishpranav@users.noreply.github.com>
2025-01-01 11:19:21 +0530

    flexstore-ios: Objective C[flexstore-ios-Bridging-Header]

--- flexstore-ios/Objective C/flexstore-ios-Bridging-Header.h
+++ flexstore-ios/Objective C/flexstore-ios-Bridging-Header.h
@@ -1,4 +1,57 @@
-//
-//  Use this file to import your target's public headers that you would like to expose to Swift.
-//
+/**
+ * @file flexstore-ios-Bridging-Header.h
+ * @author Krisna Pranav
+ * @brief Flexstore ios Bridging Header
+ * @version 1.0
+ * @date 2024-11-25
+ *
+ * @copyright Copyright (c) 2024 FlexStore Developers, Krisna Pranav
+ *
+ */
 
+#import <Foundation/Foundation.h>
+#import <UIKit/UIKit.h>
+
+extern int spawnRoot(NSString* path, NSArray* args);
+
+@class BSMonotonicReferenceTime, NSArray, NSNumber, NSString, NSURL, SBSApplicationShortcutService, SBSApplicationShortcutServiceFetchResult;
+
+@interface SBFApplication : NSObject {
+    NSURL * _bundleURL;
+}
+
+@property (nonatomic, readonly) NSURL *bundleURL;
+- (void)cxx_destruct;
+- (id)bundleURL;
+- (id)init;
+- (id)initWithApplicationBundleIdentifier:(id)arg1;
+@end
+
+@interface
+ LSApplicationWorkspace : NSObject
+- (id)allInstalledApplications;
+- (BOOL)unregisterApplication:(NSURL *)url;
+- (BOOL)registerApplication:(NSURL *)url;
+@end
+
+
+@interface
+
+ LSBundleProxy
+@property
+(readonly, nonatomic) NSURL *bundleURL;
+
+@end
+
+
+@interface
+
+ LSApplicationProxy : LSBundleProxy
+
+@end
+
+@interface UIImage (Private)
+
++(id)_applicationIconImageForBundleIdentifier:(id)arg0 format:(int)arg1 scale:(CGFloat)arg2;
+
+@end
