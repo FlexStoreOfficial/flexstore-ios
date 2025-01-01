de8cfabbae8f950090f724386352869583ca72e1
Krisna Pranav <68631244+krishpranav@users.noreply.github.com>
2025-01-01 11:12:37 +0530

    flexstore-ios: Actions[Repo]

--- flexstore-ios/Actions/Repos.swift
+++ flexstore-ios/Actions/Repos.swift
@@ -20,3 +20,21 @@ func reposEncode(reposUrl: [String]) -> String {
     
     return ""
 }
+
+func compareVersions(_ version1: String, _ version2: String) -> Bool {
+    let components1 = version1.components(separatedBy: CharacterSet(charactersIn: "."))
+    let components2 = version2.components(separatedBy: CharacterSet(charactersIn: "."))
+    
+    for (component1, component2) in zip(components1, components2) {
+        if let number1 = Int(component1), let number2 = Int(component2) {
+            if number1 != number2 {
+                return number1 > number2
+            }
+        } else {
+            
+        }
+    }
+    
+    return components1.count >= components2.count
+    
+}
