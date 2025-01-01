4c09d9b05d9c236dc304fb3d01a591df4a906292
Krisna Pranav <68631244+krishpranav@users.noreply.github.com>
2025-01-01 10:17:22 +0530

    flestore-ios: Registered + flexstore-ios-Bridging-Header

---
+++ flexstore-ios/Objective C/Registered.m
@@ -0,0 +1,12 @@
+/**
+ * @file Registered.m
+ * @author Krisna Pranav
+ * @brief Registered
+ * @version 1.0
+ * @date 2024-11-25
+ *
+ * @copyright Copyright (c) 2024 FlexStore Developers, Krisna Pranav
+ *
+ */
+
+#import <Foundation/Foundation.h>
