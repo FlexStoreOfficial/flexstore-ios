3a8ee53f966a78f6c390c750c4c481313057c9eb
Krisna Pranav <68631244+krishpranav@users.noreply.github.com>
2025-01-02 20:31:12 +0530

    flexstore-ios: Managers[ChangelogManager]

--- flexstore-ios/Managers/ChangelogManager.swift
+++ flexstore-ios/Managers/ChangelogManager.swift
@@ -6,13 +6,26 @@
 //
 
 import SwiftUI
+import BottomSheet
 
-struct ChangelogManager: View {
-    var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
-    }
+struct ListItem: Hashable {
+    var icon: String
+    var text: String
+    var subtext: String
+    var color: Color
 }
 
-#Preview {
-    ChangelogManager()
+struct ChangelogManagerView<Content: View>: View {
+    @State private var bottomSheetPosition: BottomSheetPosition = .hidden
+    @State private var page: Int32 = 0
+    @Environment(\.openURL) var openURL
+    
+    
+    
+    struct ChangelogManager: View {
+        var body: some View {
+            Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        }
+    }
+
 }
