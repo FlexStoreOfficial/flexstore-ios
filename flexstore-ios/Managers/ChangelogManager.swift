9b455495cb66e1f15156e527f83e840836f3c838
Krisna Pranav <68631244+krishpranav@users.noreply.github.com>
2025-01-02 20:29:09 +0530

    flexstore-ios: Managers[ChangelogManager]

---
+++ flexstore-ios/Managers/ChangelogManager.swift
@@ -0,0 +1,18 @@
+//
+//  ChangelogManager.swift
+//  flexstore-ios
+//
+//  Created by Krisna Pranav on 02/01/25.
+//
+
+import SwiftUI
+
+struct ChangelogManager: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ChangelogManager()
+}
