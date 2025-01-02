def63030bccdff9868173695bf7f0b3ef4c05c66
Krisna Pranav <68631244+krishpranav@users.noreply.github.com>
2025-01-02 19:36:32 +0530

    flexstore-ios: Managers[AlertManager]

--- flexstore-ios/Managers/AlertManager.swift
+++ flexstore-ios/Managers/AlertManager.swift
@@ -63,12 +63,66 @@ class AlertManager: ObservableObject {
 }
 
 
-struct AlertManager: View {
-    var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
-    }
-}
+struct AlertManagerView: View {
+    
+    @EnvironmentObject var alertManager: AlertManager
 
-#Preview {
-    AlertManager()
+    var body: some View {
+        ZStack {
+            if alertManager.isAlertPresented {
+                Color.black
+                    .ignoresSafeArea()
+                    .opacity(0.1)
+                VStack(alignment: .center) {
+                    if alertManager.showIPADetails {
+                        Text("Details Here")
+                            .bold()
+                    } else {
+                        alertManager.alertTitle
+                            .bold()
+
+                        if alertManager.showBody || alertManager.showButtons {
+                            Divider()
+                                .opacity(0.1)
+                        }
+                        
+                        if alertManager.showBody {
+                            alertManager.alertBody
+                        }
+                    }
+
+                    if alertManager.showButtons {
+                        VStack {
+                            Divider()
+
+                            Text("OK")
+                                .bold()
+
+                            .frame(maxWidth: 270)
+                            .padding(.vertical, 6)
+                        }
+                        .background(
+                            Color(.systemGray5)
+                                .opacity(0.1)
+                        )
+                        .onTapGesture {
+                           withAnimation {
+                               alertManager.isAlertPresented = false
+                           }
+                        }
+                    }
+                }
+                .padding(.horizontal, 5)
+                .padding(.vertical)
+                .frame(maxWidth: 300)
+                .background(
+                    Color(.systemGray5)
+                        .cornerRadius(16)
+                        .opacity(0.95)
+                        .blur(radius: 0.5))
+            }
+        }
+        .opacity(alertManager.isAlertPresented ? 1 : 0.25)
+        .animation(.spring().speed(2), value: alertManager.isAlertPresented)
+    }
 }
