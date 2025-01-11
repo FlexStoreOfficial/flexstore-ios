6633cadc2022db5248f166785c815d2c331f044a
Krisna Pranav <68631244+krishpranav@users.noreply.github.com>
2025-01-11 17:09:47 +0530

    flexstore-ios: Managers[UserManager]

--- flexstore-ios/Managers/UserManager.swift
+++ flexstore-ios/Managers/UserManager.swift
@@ -11,4 +11,7 @@ class UserManager: ObservableObject {
     @AppStorage("reduced_motion") var reducedMotion: Bool = true
     @AppStorage("blur_strength") var blurStrength: Double = 10
     @AppStorage("compact_repo_view") var compactRepoView: Bool = false
+    @AppStorage("lang") var lang: String = ""
+    @AppStorage("add_repo_keyboard_type") var addRepoKeyboardType: UIKeyboardType = .URL
+    @AppStorage("has_seen_discord") var hasSeenDiscord: Bool = false
 }
