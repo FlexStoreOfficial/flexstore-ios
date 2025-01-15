64373f1a4bb6bbf7329a126f0876d6a1d0ab0157
Krisna Pranav <68631244+krishpranav@users.noreply.github.com>
2025-01-15 18:57:07 +0530

    flexstore-ios: Styles[Buttons]

---
+++ flexstore-ios/Styles/Buttons.swift
@@ -0,0 +1,41 @@
+//
+//  Buttons.swift
+//  flexstore-ios
+//
+//  Created by Krisna Pranav on 15/01/25.
+//
+
+import SwiftUI
+
+public struct AppStoreStyle: ButtonStyle {
+    let type: String
+    let dissabled: Bool
+
+    public init(type: String, disabled: Bool, extraWidth: CGFloat = 0) {
+        self.type = type
+        self.dissabled = disabled
+    }
+}
+
+
+private func colors(forType type: String) -> (Color, Color) {
+    var foregroundColor: Color
+    var fill: Color
+
+    switch type {
+    case "blue":
+        foregroundColor = Color(.blue)
+        fill = Color(.blue).opacity(0.1)
+    case "green":
+        foregroundColor = Color(.green)
+        fill = Color(.green).opacity(0.1)
+    case "red":
+        foregroundColor = Color(.red)
+        fill = Color(.red).opacity(0.1)
+    default:
+        foregroundColor = Color.accentColor
+        fill = Color.gray
+    }
+
+    return (foregroundColor, fill)
+}
