0315b9576f86486f9c4623845ad79091193a8b4e
Krisna Pranav <68631244+krishpranav@users.noreply.github.com>
2025-01-16 12:55:25 +0530

    flexstore-ios: Views[Cells(RepoCell)]

--- flexstore-ios/Views/Cells/RepoCell.swift
+++ flexstore-ios/Views/Cells/RepoCell.swift
@@ -33,10 +33,31 @@ struct RepoCell: View {
                 VStack(alignment: .leading) {
                     Text(repo.data.name ?? "UNNAMED_REPO")
                     if (!userSettings.compactRepoView) {
-                        
+
                     }
                 }
+
+                Spacer()
+
+                VStack(alignment: .center) {
+                    Text("\(repo.data.apps.count)")
+                        .font(.callout)
+                        .foregroundStyle(.gray)
+                        .opacity(0.75)
+                }
+                .padding(.trailing, 15)
             }
+            .frame(maxWidth: .infinity, alignment: .leading)
+            .padding(.init(top: 10, leading: 15, bottom: 10, trailing: 15))
+            .background(
+                NavigationLink("", destination: SourceView(repo: repo.data))
+                    .padding(.trailing, 15)
+                    .opacity(0)
+            )
+
+            .background(
+                Color.white.opacity(0.001)
+            )
         }
     }
 }
