56eae5add7b17bdfb3a5edfdbe5c155d973f42aa
Krisna Pranav <68631244+krishpranav@users.noreply.github.com>
2025-01-16 09:16:59 +0530

    flexstore-ios: Views[Cells(AppCell)]

---
+++ flexstore-ios/Views/Cells/AppCell.swift
@@ -0,0 +1,14 @@
+//
+//  AppCell.swift
+//  flexstore-ios
+//
+//  Created by Krisna Pranav on 16/01/25.
+//
+
+import SwiftUI
+
+struct AppCell: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
