54f718fe9e6350b97dde9550ac1e66d0b41405a5
Krisna Pranav <68631244+krishpranav@users.noreply.github.com>
2025-01-16 12:51:41 +0530

    flexstore-ios: Views[Cells(RepoCell)]

---
+++ flexstore-ios/Views/Cells/RepoCell.swift
@@ -0,0 +1,14 @@
+//
+//  RepoCell.swift
+//  flexstore-ios
+//
+//  Created by Krisna Pranav on 16/01/25.
+//
+
+import SwiftUI
+
+struct RepoCell: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
