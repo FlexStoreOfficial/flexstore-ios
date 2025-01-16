36f5b36cf62bf84d30fa57fe8b41c06c085cc0e6
Krisna Pranav <68631244+krishpranav@users.noreply.github.com>
2025-01-16 09:18:45 +0530

    flexstore-ios: Views[Cells(AppCell)]

--- flexstore-ios/Views/Cells/AppCell.swift
+++ flexstore-ios/Views/Cells/AppCell.swift
@@ -6,9 +6,26 @@
 //
 
 import SwiftUI
+import Kingfisher
 
 struct AppCell: View {
+    let app: Application
+    let showFullMode: Bool
+
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        let version = (app.versions?[0].version != nil && app.versions?[0].version != "" ? (app.versions?[0].version ?? "") : "")
+
+        VStack(alignment: .leading) {
+            HStack {
+                if (app.iconURL != "") {
+                    KFImage(URL(string: app.iconURL)).resizable().frame(width: 48, height: 48).padding(.trailing, 7)
+                } else {
+                    Image("MissingApp")
+                        .resizable()
+                        .frame(width: 48, height: 48)
+                        .padding(.trailing, 7)
+                }
+            }
+        }
     }
 }
