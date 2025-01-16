8ff401ce7c6c1e4c0339b0edebcefb526135a7ae
Krisna Pranav <68631244+krishpranav@users.noreply.github.com>
2025-01-16 09:21:15 +0530

    flexstore-ios: Views[Cells(AppCell)]

--- flexstore-ios/Views/Cells/AppCell.swift
+++ flexstore-ios/Views/Cells/AppCell.swift
@@ -9,34 +9,54 @@ import SwiftUI
 import Kingfisher
 
 struct AppCell: View {
-    let app: Application
+    let app : Application
     let showFullMode: Bool
 
     var body: some View {
         let version = (app.versions?[0].version != nil && app.versions?[0].version != "" ? (app.versions?[0].version ?? "") : "")
+        let devName = (app.developerName != nil && app.developerName != "" ? (app.developerName ?? "") : "")
 
         VStack(alignment: .leading) {
             HStack {
-                if (app.iconURL != "") {
-                    KFImage(URL(string: app.iconURL)).resizable().frame(width: 48, height: 48).padding(.trailing, 7)
+                if(app.iconURL != "") {
+                    KFImage(URL(string: app.iconURL))
+                        .resizable()
+                        .frame(width: 48, height: 48)
+                        .clipShape(RoundedRectangle(cornerRadius: 10))
+                        .padding(.trailing, 7)
+
                 } else {
                     Image("MissingApp")
                         .resizable()
                         .frame(width: 48, height: 48)
+                        .clipShape(RoundedRectangle(cornerRadius: 10))
                         .padding(.trailing, 7)
                 }
+
+                VStack(alignment: .leading) {
+                    Text(app.name)
+                    CollapsibleText(text: devName , isExpanded: .constant(false), maxLines: 1)
+                        .font(.caption)
+                        .foregroundColor(.gray)
+                    CollapsibleText(text: version , isExpanded: .constant(false), maxLines: 1)
+                        .font(.caption)
+                        .foregroundColor(.gray)
+                }
+                Spacer()
+                DynamicInstallButton(appDetails: app, selectedVersionIndex: 0, buttonStyle: "Main")
             }
 
             if showFullMode {
                 let screenshots = app.screenshotURLs ?? []
-                if (screenshots.count > 0) {
+                if(screenshots.count > 0 ) {
                     ScrollView(.horizontal, showsIndicators: false) {
                         HStack(spacing: 10) {
                             ForEach(screenshots.indices, id: \.self) { index in
                                 KFImage(URL(string: screenshots[index]))
                                     .resizable()
                                     .aspectRatio(contentMode: .fill)
-                                    .clipShape(RoundedRectangle(cornerRadius: 16))
+                                    .frame(height: 250)
+                                    .clipShape(RoundedRectangle(cornerRadius: 15))
                             }
                         }
                         .padding(.horizontal, 10)
@@ -47,7 +67,8 @@ struct AppCell: View {
         .frame(maxWidth: .infinity, alignment: .leading)
         .padding(.init(top: 10, leading: 15, bottom: 10, trailing: 15))
         .background(
-            NavigationLink("", destination: TitleContent()).opacity(0)
+            NavigationLink("", destination: AppDetailsView(appDetails: app))
+                .opacity(0)
         )
     }
 }
