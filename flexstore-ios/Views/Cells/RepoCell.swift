992fd74a4b50521b40cda9d39391d5fad6e13bda
Krisna Pranav <68631244+krishpranav@users.noreply.github.com>
2025-01-16 12:53:56 +0530

    flexstore-ios: Views[Cells(RepoCell)]

--- flexstore-ios/Views/Cells/RepoCell.swift
+++ flexstore-ios/Views/Cells/RepoCell.swift
@@ -5,10 +5,38 @@
 //  Created by Krisna Pranav on 16/01/25.
 //
 
+import Kingfisher
 import SwiftUI
+import SwipeActions
 
 struct RepoCell: View {
+    @EnvironmentObject var userSettings: UserSettings
+
+    let repo: RepoMemory
+    let removeRepos: (Set<UUID>) -> Void
+
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        SwipeView {
+            HStack {
+                let iconSize: CGFloat = userSettings.compactRepoView ? 35 : 48
+
+                if(repo.data.iconURL == nil || repo.data.iconURL == "") {
+                    Image("MissingRepo")
+                        .resizable()
+                        .frame(width: iconSize, height: iconSize)
+                        .clipShape(RoundedRectangle(cornerSize: 25))
+                        .padding(.trailing, 7)
+                } else {
+
+                }
+
+                VStack(alignment: .leading) {
+                    Text(repo.data.name ?? "UNNAMED_REPO")
+                    if (!userSettings.compactRepoView) {
+                        
+                    }
+                }
+            }
+        }
     }
 }
