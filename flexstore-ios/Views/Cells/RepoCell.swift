6b386c6fcc3d512e3e140f1fd0af2d618faba7b8
Krisna Pranav <68631244+krishpranav@users.noreply.github.com>
2025-01-16 12:55:59 +0530

    flexstore-ios: Views[Cells(RepoCell)]

--- flexstore-ios/Views/Cells/RepoCell.swift
+++ flexstore-ios/Views/Cells/RepoCell.swift
@@ -5,35 +5,41 @@
 //  Created by Krisna Pranav on 16/01/25.
 //
 
-import Kingfisher
-import SwiftUI
 import SwipeActions
+import SwiftUI
+import Kingfisher
 
 struct RepoCell: View {
     @EnvironmentObject var userSettings: UserSettings
 
-    let repo: RepoMemory
+    let repo : RepoMemory
     let removeRepos: (Set<UUID>) -> Void
 
     var body: some View {
         SwipeView {
             HStack {
-                let iconSize: CGFloat = userSettings.compactRepoView ? 35 : 48
+                let iconSize : CGFloat = userSettings.compactRepoView ? 35 : 48
 
                 if(repo.data.iconURL == nil || repo.data.iconURL == "") {
                     Image("MissingRepo")
                         .resizable()
                         .frame(width: iconSize, height: iconSize)
-                        .clipShape(RoundedRectangle(cornerSize: 25))
+                        .clipShape(RoundedRectangle(cornerRadius: 25))
                         .padding(.trailing, 7)
                 } else {
-
+                    KFImage(URL(string: repo.data.iconURL ?? ""))
+                        .resizable()
+                        .frame(width: iconSize, height: iconSize)
+                        .clipShape(RoundedRectangle(cornerRadius: 25))
+                        .padding(.trailing, 7)
                 }
 
                 VStack(alignment: .leading) {
                     Text(repo.data.name ?? "UNNAMED_REPO")
                     if (!userSettings.compactRepoView) {
-
+                        CollapsibleText(text: repo.url, isExpanded: .constant(false), maxLines: 2)
+                            .font(.caption)
+                            .foregroundColor(.gray)
                     }
                 }
 
@@ -42,7 +48,7 @@ struct RepoCell: View {
                 VStack(alignment: .center) {
                     Text("\(repo.data.apps.count)")
                         .font(.callout)
-                        .foregroundStyle(.gray)
+                        .foregroundColor(.gray)
                         .opacity(0.75)
                 }
                 .padding(.trailing, 15)
@@ -52,12 +58,45 @@ struct RepoCell: View {
             .background(
                 NavigationLink("", destination: SourceView(repo: repo.data))
                     .padding(.trailing, 15)
-                    .opacity(0)
             )
-
             .background(
                 Color.white.opacity(0.001)
             )
+            .contextMenu
+            {
+                Button(action: {
+                    UIPasteboard.general.string = repo.url
+                }, label:
+                        {
+                    Text("COPY_REPO_URL")
+                })
+                Button(action: {
+                    UIPasteboard.general.string = reposEncode(reposUrl: [repo.url])
+                }, label:
+                        {
+                    Text("COPY_REPO_BASE_URL")
+                })
+                Button(action: {
+                    withAnimation {
+                        removeRepos([repo.id])
+                    }
+                }, label:
+                        {
+                    Text("DELETE_REPO")
+                })
+            }
+        } trailingActions: { _ in
+            SwipeAction(systemImage: "trash", backgroundColor: .pink, highlightOpacity: 1) {
+                withAnimation {
+                    removeRepos([repo.id])
+                }
+            }
+            .allowSwipeToTrigger()
         }
+        .swipeActionsStyle(.mask)
+        .swipeMinimumDistance(35)
+        .swipeActionCornerRadius(0)
+        .swipeSpacing(0)
+        .swipeActionsMaskCornerRadius(0)
     }
 }
